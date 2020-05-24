7f55e2f9cf90b7af624fb43c9f425fdb0288e9d6
Chris Wong <wc38.wong@gmail.com>
2020-05-24 12:21:12 +0800

    Day 04-React的靈魂角色(Components)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # [React 30天 系列](https://ithelp.ithome.com.tw/users/20111595/ironman/1643)
 - [Day 02-打造個人工作室(開發環境建置)](https://ithelp.ithome.com.tw/articles/10200458)
 - [Day 03-其實我只是披著html的javascript(JSX)](https://ithelp.ithome.com.tw/articles/10200539)
+- [Day 04-React的靈魂角色(Components)](https://ithelp.ithome.com.tw/articles/10200775)
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from './src/components/button';
 
 const App = () => {
   return (
-    React.createElement('div', null, 'Hello World!')
+    <div>
+      Hello World!
+      <Button text="按鈕按鈕"/>
+      <Button text="按鈕排序二"/>
+      <Button text="按鈕排序三"/>
+      <Button text="按鈕排序四"/>
+    </div>
   );
 };
 
