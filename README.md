5ca325b9baa14acd9acd3e63d576d817f8bcb1d3
Chris Wong <wc38.wong@gmail.com>
2020-05-25 07:54:20 +0800

    React Native教學 Part 4.5 - This Binding和Arrow Function（箭頭函式）

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - [Day 03-其實我只是披著html的javascript(JSX)](https://ithelp.ithome.com.tw/articles/10200539)
 - [Day 04-React的靈魂角色(Components)](https://ithelp.ithome.com.tw/articles/10200775)
 - [Day 05-靈魂角色的家庭成員(Props & State)](https://ithelp.ithome.com.tw/articles/10200878)
+
+# [React Native教學 Part 4.5 - This Binding和Arrow Function（箭頭函式）](https://carsonwah.github.io/react-native-arrow-function-and-this.html)
--- src/components/counter.js
+++ src/components/counter.js
@@ -6,10 +6,9 @@ class Counter extends Component { // 1. 建立Class Component
     this.state = {  
       count: 0
     };
-    this.addCount = this.addCount.bind(this);  // 6. 綁定component instance
   }
 
-  addCount() {  // 5. 處理click event
+  addCount = () => {  // 5. 處理click event
     this.setState({ count: this.state.count + 1 });
   }
 
@@ -17,7 +16,7 @@ class Counter extends Component { // 1. 建立Class Component
     return (
       <div>
         <h1>{this.state.count}</h1>
-        <button onClick={this.addCount}>點擊+1</button> {/* 4.設定點擊事件 */}
+        <button onClick={this.addCount}>點擊加一</button> {/* 4.設定點擊事件 */}
       </div>
     );
   }
