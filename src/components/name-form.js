c017d6c8a4cee93ad43b245a5aac426b7a7ff921
Chris Wong <wc38.wong@gmail.com>
2020-05-26 18:06:47 +0800

    Controlled Components

--- src/components/name-form.js
+++ src/components/name-form.js
@@ -3,12 +3,20 @@ import React from "react";
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      textValue: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.input = React.createRef();  // 2. 使用React.createRef()
+  }
+
+  handleChange(event) {
+    this.setState({'textValue': event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.input.current.value); // 3. 取得ref
+    alert('A name was submitted: ' + this.state.textValue); // 3. 取得ref
     event.preventDefault(); // 昨天講到的避免瀏覽器默認行為，這邊的默認行為是form submit
   }
 
@@ -17,7 +25,7 @@ class NameForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" ref={this.input} /> {/* 1. 設定ref屬性 */}
+          <input type="text" onChange={this.handleChange} value={this.state.textValue} /> {/* 1. 設定ref屬性 */}
         </label>
         <input type="submit" value="Submit" />
       </form>
