8c3ffb45afc3e0bedd441563577940fd53660223
Chris Wong <wc38.wong@gmail.com>
2020-05-26 18:10:11 +0800

    處理多個input element

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NameForm from "./src/components/name-form";
+import Contact from "./src/components/contact";
 
 const App = () => {
   return (
     <div>
-     <NameForm />
+     <Contact />
     </div>
   );
 };
---
+++ src/components/contact.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      address: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const { value, name } = target;
+
+    this.setState({
+      [name]: target.value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          姓名
+          <input
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleInputChange} />
+        </label>
+        <label>
+          地址
+          <input
+            name="address"
+            type="text"
+            value={this.state.address}
+            onChange={this.handleInputChange} />
+        </label>
+        <hr/>
+        <div>
+          {this.state.name} <br/>
+          {this.state.address}
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Contact;
